2589a7081ce03d78006b38b3ae841f3adbace517
aishwaryatamilselvan19-hub <aishwaryatamilselvan19@gmail.com>
2026-08-27 14:01:19 +0530

    Add conditional execution logic in Conditional execution.py

---
+++ Conditional execution.py
@@ -0,0 +1,24 @@
+a=5
+b=6
+if a>0:
+    print("a is positive")
+if b>0:
+    print("b is positive")
+    a=76
+    print("a is now", a)
+    a=98
+    b=45
+    if a>b:
+        print("a is greater than b")
+        a= 100
+        b= 200
+        if a>b:
+            print("a is greater than b")
+            a=23,45,35,56,76
+            b= 45,67,89,90,34
+            if a>b:
+                print("a is greater than b")
+                a=23,87,45,67,89
+                b= 45,67,89,90,34
+                if a>b:
+                    print("a is greater than b")
